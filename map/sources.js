ca15309bb2900b50265a6d0eb9f3b7aefdb00110
Islen Vallejo-Henao <iv@nilu.no>
2025-02-05 10:31:14 +0100

    Move source.js to ./map; make layers keys str sources.js

---
+++ map/sources.js
@@ -0,0 +1,56 @@
+var externalData = {
+    sources: {
+        landsat: {
+            tiles: [
+                "http://localhost:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=/app/data/landsat.tiff&bidx=1&expression=b1&colormap_name=coolwarm"
+            ],
+            type: "raster",
+            tileSize: 256,
+            maxzoom: 12
+        },
+        harvey: {
+            tiles: [
+                "http://localhost:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=/app/data/harvey.tiff&bidx=1&expression=b1&colormap_name=coolwarm"
+            ],
+            type: "raster"
+        },
+        libya: {
+            tiles: [
+                "http://localhost:8000/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@1x?url=/app/data/libya.tiff&bidx=1&expression=b1&colormap_name=coolwarm"
+            ],
+            type: "raster"
+        }
+    },
+    layers: [
+        {
+            id: "landsat-tiles",
+            type: "raster",
+            source: "landsat",
+            minzoom: 7,
+            maxzoom: 12,
+            paint: {
+                "raster-opacity": 0.7  // The property name still requires quotes if it contains a hyphen
+            }
+        },
+        {
+            id: "harvey-tiles",
+            type: "raster",
+            source: "harvey",
+            minzoom: 10,
+            maxzoom: 15,
+            paint: {
+                "raster-opacity": 0.7  // Same here
+            }
+        },
+        {
+            id: "libya-tiles",
+            type: "raster",
+            source: "libya",
+            minzoom: 12,
+            maxzoom: 19,
+            paint: {
+                "raster-opacity": 0.7  // Same here
+            }
+        }
+    ]
+};
